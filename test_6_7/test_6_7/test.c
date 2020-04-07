4265cfc009d4912e94b3c6358f0a056efde5f47b
zhangpeixiang <1257630257@qq.com>
2020-04-08 00:28:05 +0800

    冒泡排序

---
+++ test_6_7/test_6_7/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//ц╟ещеепР
+void BubbleSort(int* arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+	for (j = 0; j < sz; j++)
+	{
+		for (i = 0; i < sz-j; i++)
+		{
+			int ret = 0;
+			if (arr[i] > arr[i + 1])
+			{
+				ret = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = ret;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int i = 0;
+	int arr[] = { 2, 4, 5, 6, 8, 9, 3, 0, 1, 7 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSort(arr, sz);
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
+
+
