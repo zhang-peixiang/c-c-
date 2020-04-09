a7bdbed31c146bbee0cf0af89c4bc637a586ac7a
zhangpeixiang <1257630257@qq.com>
2020-04-09 21:14:55 +0800

    最小公倍数

---
+++ test_4_9 测试/test_4_9 测试/test.c.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//最小公倍数
+//int print(int A, int B)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= B; i++)
+//	{
+//		for (j = 1; j <= A; j++)
+//		{
+//			int c = A*i;
+//			if (c == B*j)
+//			{
+//				return c;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int A = 0;
+//	int B = 0;
+//	int ret = 0;
+//	scanf("%d%d", &A, &B);
+//	ret = print(A, B);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
