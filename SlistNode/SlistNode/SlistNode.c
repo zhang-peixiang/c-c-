7f7a230d4799042ca4119407b5aa5a41a81969ea
zhangpeixiang <1257630257@qq.com>
2020-06-02 22:54:20 +0800

    链表的基本操作

--- SlistNode/SlistNode/SlistNode.c
+++ SlistNode/SlistNode/SlistNode.c
@@ -18,7 +18,7 @@ SlistNode* BuySlistNode(SLDataType data)
 
 
 //尾插
-void SlistPshBack(SlistNode** head, SLDataType data)
+void SlistPushBack(SlistNode** head, SLDataType data)
 {
 	assert(head);
 	SlistNode* newNode = BuySlistNode(data);
@@ -46,6 +46,11 @@ void SlistPopBack(SlistNode** head)
 	if (*head == NULL)
 	{
 		return NULL;
+	}
+	else if ((*head)->next == NULL)
+	{
+		free(*head);
+		*head = NULL;
 	}
 	else
 	{
@@ -81,6 +86,9 @@ void SlistPushFront(SlistNode** head, SLDataType data)
 	//	*head = newNode;
 	//}
 }
+
+
+//头删
 void SlistPopFront(SlistNode** head)
 {
 	assert(head);
@@ -95,12 +103,147 @@ void SlistPopFront(SlistNode** head)
 	free(delNode);
 }
 
-void SlistInsert(SlistNode* pos, SLDataType data);
-void SlistErase(SlistNode* pos);
+//任意位置插入
+void SlistInsert(SlistNode* pos, SLDataType data)
+{
+	if (pos == NULL)
+	{
+		return;
+	}
+	SlistNode* newNode = BuySlistNode(data);
+	newNode->next = pos->next;
+	pos->next = newNode;
 
-int SlistSize(SlistNode* head);
-int SlistEmpty(SlistNode* head);
+}
 
-SlistNode* SistFind(SlistNode* head, SLDataType data);
+//任意位置删除
+void SlistErase(SlistNode* pos)
+{
+	if (pos == NULL || pos->next==NULL)
+	{
+		return;
+	}
+	SlistNode* delNode = pos->next;
+	pos->next = delNode->next;
+	free(delNode);
+}
 
-void SListDestroy(SlistNode** head);
+int SlistSize(SlistNode* head)
+{
+	int count = 0;
+	SlistNode* cur = head;
+	while (cur)
+	{
+		count++;
+		cur = cur->next;
+	}
+	return count;
+}
+int SlistEmpty(SlistNode* head)
+{
+	return head == NULL;
+}
+
+SlistNode* SlistFind(SlistNode* head, SLDataType data)
+{
+	SlistNode* cur = head;
+	while (cur)
+	{
+		if (cur->date == data)
+		{
+			return cur;
+		}
+		cur = cur->next;
+	}
+	return NULL;
+}
+
+void SListDestroy(SlistNode** head)
+{
+	assert(head);
+	while (*head)
+	{
+		SlistNode* delNode = *head;
+		*head = delNode->next;
+		free(delNode);
+	}
+}
+
+void PrintSList(SlistNode* head)
+{
+	SlistNode* cur = head;
+	while (cur)
+	{
+		printf("%d--->", cur->date);
+		cur = cur->next;
+	}
+	printf("NULL\n");
+}
+
+void TestSList3()
+{
+	SlistNode* listhead = NULL;
+	SlistPushFront(&listhead, 1);
+	SlistPushFront(&listhead, 2);
+	SlistPushFront(&listhead, 3);
+	SlistPushFront(&listhead, 4);
+	SlistPushFront(&listhead, 5);
+	PrintSList(listhead);
+
+	SlistInsert(SlistFind(listhead,1), 0);
+	PrintSList(listhead);
+
+	SlistErase(SlistFind(listhead, 1));
+	PrintSList(listhead);
+}
+
+void TestSList2()
+{
+	SlistNode* listhead = NULL;
+	SlistPushFront(&listhead, 1);
+	SlistPushFront(&listhead, 2);
+	SlistPushFront(&listhead, 3);
+	SlistPushFront(&listhead, 4);
+	SlistPushFront(&listhead, 5);
+	PrintSList(listhead);
+
+	SlistPopFront(&listhead);
+	SlistPopFront(&listhead);
+	SlistPopFront(&listhead);
+	SlistPopFront(&listhead);
+	SlistPopFront(&listhead);
+	SlistPopFront(&listhead);
+	PrintSList(listhead);
+
+}
+
+void TestSList1()
+{
+	SlistNode* listhead = NULL;
+	SlistPushBack(&listhead, 1);
+	SlistPushBack(&listhead, 2);
+	SlistPushBack(&listhead, 3);
+	SlistPushBack(&listhead, 4);
+	SlistPushBack(&listhead, 5);
+	PrintSList(listhead);
+
+
+	SlistPopBack(&listhead);
+	SlistPopBack(&listhead);
+	SlistPopBack(&listhead);
+	SlistPopBack(&listhead);
+	SlistPopBack(&listhead);
+	SlistPopBack(&listhead);
+
+	PrintSList(listhead);
+}
+
+
+
+void TestSList()
+{
+	//TestSList1();
+	//TestSList2();
+	TestSList3();
+
+}
