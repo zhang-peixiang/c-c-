36f8fa84014efcc8a857d8d6656e1a8db8e3114e
zhangpeixiang <1257630257@qq.com>
2020-07-09 19:46:59 +0800

    堆操作（加入函数指针实现一个函数可以实现大小堆的操作）

--- Heap.h/Heap.h/heap.h
+++ Heap.h/Heap.h/heap.h
@@ -3,18 +3,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define HDataType int 
+typedef int HDataType;
+
+
+//加入函数指针 可以控制创建大堆还是小堆
+int Less(HDataType left, HDataType right);
+int Greater(HDataType left, HDataType right);
+
+int (*cmp)(HDataType Less, HDataType Greater);
+
+typedef int(*CMP)(HDataType, HDataType);
+
+
 
 typedef struct Heap
 {
 	HDataType* array;
 	int capacity;
 	int size;
+	CMP cmp;
 }Heap;
 
 
-void HeapInit(Heap* hp, int initCap);
-void HeapCreate(Heap* hp, int* array, int size);
+void HeapInit(Heap* hp, int initCap,CMP cmp);
+void HeapCreate(Heap* hp, int* array, int size,CMP cmp);
 void HeapPush(Heap* hp, HDataType data);
 void HeapPop(Heap* hp);
 HDataType HeapTop(Heap* hp);
